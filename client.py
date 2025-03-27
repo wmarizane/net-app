13e5fc55418d6c6960a5963c2b7e1d149926624d
NGUYEN ANH KHOA TRAN <trannguyenanhkhoa05122003@gmail.com>
2025-03-27 02:15:57 -0500

    New server/client version

--- client.py
+++ client.py
@@ -1,50 +1,382 @@
 import socket
+import sys
 import threading
-import textwrap
+import json
+import random
+import queue
+import re
 
-HOST = '127.0.0.1'
-PORT = 65432
-BUBBLE_WIDTH = 40
+IP = "127.0.0.1"
+PORT = 12000
+HEADER_LENGTH = 2048
 
-def speech_bubble(message, sender):
-    lines = textwrap.wrap(message, width=BUBBLE_WIDTH)
-    max_length = max(len(line) for line in lines)
-    
-    top = " " + "_" * (max_length + 2)
-    # middle = "\n".join([f"/ {line.ljust(max_length)} \"] + [f"| {line.ljust(max_length)} |" for line in wrapped_lines[1:]])
-    middle = "/ " + lines[0].ljust(max_length) + " \\"  # First line with slashes
-    middle += "\n" + "\n".join([f"| {line.ljust(max_length)} |" for line in lines[1:]]) 
-    bottom = f"\\_{'_' * max_length}_/\n |/\n {sender}"
-    
-    return f"{top}\n{middle}\n{bottom}"
+'''
+MESSAGE FORMAT
+    {
+        'sender'
+        'receiver'
+        'content'
+        'ID'
+        'method' {
+            'action'
+            'ID'
+        }
+    }
+'''
 
-def receive_messages(sock):
+CLIENT_LIST = []
+lock = threading.Lock()
+input_ready_event_dict = {}
+
+
+MESSAGE_DICT = {}
+#SENDING_QUEUE = queue.Queue()
+
+def receive_message(socket):
+    '''
+        socket: 
+    '''
+    data = socket.recv(HEADER_LENGTH).decode('utf-8')
+    return json.loads(data)
+
+def send_message(socket,message):
+    '''
+        socket: socket of receiver
+    '''
+    socket.send(json.dumps(message).encode('utf-8'))
+
+def generate_message_ID(clientID):
     while True:
-        try:
-            msg = sock.recv(1024).decode()
-            if not msg:
-                break
-            sender, message = msg.split(":", 1)
-            print(speech_bubble(message.strip(), sender))
-        except:
-            break
+        # Generate a random number between 1000 and 9999
+        random_number = random.randint(1000, 9999)
+        
+        # Create a unique ID by combining name and random number
+        message_ID = f"{clientID}/{random_number}"
+        
+        # Check if the ID is unique in the clients dictionary
+        if message_ID not in MESSAGE_DICT:
+            return message_ID    
 
-def start_client():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect((HOST, PORT))
-        print(client.recv(1024).decode(), end="")  # Prompt for name
-        name = input().strip()
-        client.sendall(name.encode())
-        
-        threading.Thread(target=receive_messages, args=(client,), daemon=True).start()
-        
-        print("Typer your messages using 'recipient: message format. Type '.exit' to quit.") 
-        while True:
-            msg = input()
-            if msg.lower() == '.exit':
+def register_client_ID(client_socket):
+
+    message = receive_message(client_socket)
+    method_action = message.get('method',{}).get('action')
+    content = message.get('content')
+    sender = message.get('sender')
+
+    if method_action == 'NEW' and sender == 'server':
+        #Print the prompt and get user name
+        user_name =input(content + ": ").strip()
+
+        message = {
+        'sender' : user_name,
+        'receiver': ['server'],
+        'content': user_name,
+        'ID': None,
+        'method': {
+            'action' : 'NEW',
+            'ID' : None,
+            }
+        }
+    
+    send_message(client_socket,message=message)
+
+    message = receive_message(client_socket)
+
+    client_ID = message.get('receiver')[0]
+
+    input_ready_event_dict[client_ID] = threading.Event()
+
+    return client_ID
+
+
+def display_message(message):
+    sender = message.get("sender", "Unknown")
+    receivers = ", ".join(message.get("receiver", [])) if isinstance(message.get("receiver"), list) else "Unknown"
+    content = message.get("content", "No content")
+    msg_id = message.get("ID", "No ID")
+    method = message.get("method", {})
+    method_action = method.get("action", "Unknown")
+    method_id = method.get("ID", "No method ID")
+    print('\n')
+    print("----- Message Details -----")
+    print(f"Sender:      {sender}")
+    print(f"Receivers:   {receivers}")
+    print(f"Content:     {content}")
+    print(f"Message ID:  {msg_id}")
+    print("----- Method Details ------")
+    print(f"Method Name: {method_action}")
+    print(f"Method ID:   {method_id}")
+    print("----------------------------")
+    print('\n')
+
+def display_reply_message(reply_message):
+    sender = reply_message.get("sender", "Unknown")
+    receivers = ", ".join(reply_message.get("receiver", [])) if isinstance(reply_message.get("receiver"), list) else "Unknown"
+    content = reply_message.get("content", "No content")
+    mssg_id = reply_message.get("ID", "No ID")
+    method = reply_message.get("method", {})
+    method_action = method.get("action", "Unknown")
+    reply_id = method.get("ID", "No method ID")
+    
+    print('\n')
+    print("----- Reply Message Details -----")
+    print(f"Sender:      {sender}")
+    print(f"Receivers:   {receivers}")
+    print(f"Content:     {content}")
+    print(f"Reply of Message ID:    {reply_id}")
+    print(f"Message ID:  {mssg_id}")
+    print("----- Method Details ------")
+    print(f"Method Name: {method_action}")
+    print("----------------------------")
+    print('\n')
+
+
+def display_client_list():
+    """
+    Display the updated client list.
+    """
+    global CLIENT_LIST
+
+    print("\n----- Connected Clients -----")
+    if CLIENT_LIST:
+        for i, client in enumerate(CLIENT_LIST, start=1):
+            print(f"{i}. {client}")
+    else:
+        print("No clients connected.")
+    print("-----------------------------\n")
+
+def parse_create_message(str,clientID,mssg_ID):
+    headers_and_content = str.split(':')
+    headers = [header for header in headers_and_content[0].split(' ') if header]
+    content = headers_and_content[1].strip()
+
+    message = {
+        'sender' : None,
+        'receiver': None,
+        'content': content,
+        'ID': None,
+        'method': {
+            'action' : None,
+            'ID' : None,
+            }
+    }
+
+    action = headers[0]
+
+    if headers[0] == 'REPLY' or headers[0] == 'DEL':
+        opt_ID = headers[-1]
+        receivers = headers[1:len(headers)-1]
+        message['method']['ID'] = opt_ID
+    else:
+        receivers = headers[1:]
+    
+    message['receiver'] = receivers
+    message['sender'] = clientID
+    message['ID'] = mssg_ID
+    message['method']['action'] = action
+
+    return message
+
+
+def handling_messages(client_socket,client_ID, shutdown_event):
+    global CLIENT_LIST
+    while not shutdown_event.is_set():
+        try:
+            message = receive_message(client_socket)
+            action = message['method']['action']
+            if action == 'POST':
+
+                #Update receiving message
+                with lock:
+                    message_ID = message['ID']
+                    MESSAGE_DICT[message_ID] = {
+                        'message' : message,
+                        'status' : True,
+                    }
+
+                    # ack_message = {
+                    #     'sender' : client_ID,
+                    #     'receiver': [message['sender']],
+                    #     'content': None,
+                    #     'ID': None,
+                    #     'method': {
+                    #         'action' : 'ACK_MSSG',
+                    #         'ID' : message_ID,
+                    #     }
+                    # }
+
+                    # #Push the ACK to queue
+                    # SENDING_QUEUE.put(ack_message)
+
+                display_message(message=message)
+                print('You >')
+                
+            elif action == 'UPDATE_CLIENT_LIST':
+                # Update the global client list
+                with lock:
+                    clist = message['content'].split(', ')
+                    CLIENT_LIST = clist
+                    print("\nClient list updated:")
+                    display_client_list()
+                    print('You >')
+            elif action == 'REPLY':
+                with lock:
+                    reply_mssg_ID = message['method']['ID']
+
+                    if reply_mssg_ID in MESSAGE_DICT.keys():
+                        if MESSAGE_DICT[reply_mssg_ID]['status'] == True:
+                            MESSAGE_DICT[reply_mssg_ID] = message
+                            display_reply_message(reply_message=message)
+                        else:
+                            print('The replied message is NOT available any more')
+                            # message = {
+                            #     'sender' : client_ID,
+                            #     'receiver': [message['sender']],
+                            #     'content': None,
+                            #     'ID': None,
+                            #     'method': {
+                            #         'action' : 'REP_FAIL',
+                            #         'ID' : reply_mssg_ID,
+                            #     }
+                            # }
+                            # #Push the warning to queue
+                            # SENDING_QUEUE.put(message)
+                print('You >')
+            elif action == 'DELETE':
+                with lock:
+                    del_mssg_ID = message['method']['ID']
+                    if del_mssg_ID in MESSAGE_DICT.keys():
+                        if MESSAGE_DICT[del_mssg_ID]['status'] == True:
+                            MESSAGE_DICT[del_mssg_ID]['status'] == False
+
+                            # message = {
+                            #     'sender' : client_ID,
+                            #     'receiver': [message['sender']],
+                            #     'content': None,
+                            #     'ID': None,
+                            #     'method': {
+                            #         'action' : 'ACK_DEL',
+                            #         'ID' : del_mssg_ID,
+                            #     }
+                            # }
+
+                            #Push the ACK to queue
+                            #SENDING_QUEUE.put(message)
+
+            # elif action == 'REP_FAIL':
+            #     print('** Cannot reply message {} to {} **'.format(message['method']['ID'],message['sender']))
+            # elif re.match(r"ACK_.*",action):
+            #     # ACK_ is used for acknowledgement
+            #     if action == 'ACK_MSSG':
+            #         print('** Message {} has been received by {} **'.format(message['method']['ID'],message['sender']))
+            #     elif action == 'ACK_DEL':
+            #         print('** Message {} has been deleted by {} **'.format(message['method']['ID'],message['sender']))
+            #     elif action == 'ACK_REP':
+            #         print('** Replied message {} has been received by {} **'.format(message['method']['ID'],message['sender']))   
+            else:
+                print('Cannot found the action for the message')
+            input_ready_event_dict[client_ID].set()
+
+        except Exception as e:
+            if not shutdown_event.is_set(): # Only print error if it wasn't a shutdown.
+                print(f"Error receiving message: {e}")
                 break
-            client.sendall(msg.encode())
+            # print(f"Error receiving message: {e}")
+            # sys.exit()
+
+# def send_queued_messages(client_socket):
+#     while True:
+#         with lock:
+#         #Send all the receipt message back to the sender
+#             outgoing_message = SENDING_QUEUE.get()
+#             if not outgoing_message:
+#                 break
+#             send_message(client_socket,outgoing_message)
+
+
+def main():
+
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    shutdown_event = threading.Event()
+
+    try:
+        # Connect to the server
+        client_socket.connect((IP, PORT))
+        print(f"Connected to {IP}:{PORT}")
+    except Exception as e:
+        print(f"Connection failed: {e}")
+        sys.exit()
+
+
+
+    client_ID = register_client_ID(client_socket=client_socket)
+    print(client_ID)
+
+    #Create thread to receive all the messages from other clients
+    message_receiver = threading.Thread(target=handling_messages, args=(client_socket,client_ID,shutdown_event), daemon=True)
+    message_receiver.start()
+
+    # queue_handler = threading.Thread(target=send_queued_messages, args=(client_socket,), daemon=True)
+    # queue_handler.start()
+
+    input_ready_event_dict[client_ID].wait()
+    #Send the message to the server
+    while True:
+        content = input('You > ')
+
+        # INPUT format
+        # ACTION
+
+        #generate ID for the new message
+        message_ID = generate_message_ID(clientID=client_ID)
+
+        message = {
+        'sender' : client_ID,
+        'receiver': None,
+        'content': content,
+        'ID': message_ID,
+        'method': {
+            'action' : None,
+            'ID' : None,
+            }
+        }
+
+        if content.lower() == ".exit":
+            #Hanlde user input ".exit" to leave the chat
+    
+            with lock:
+                message['method']['action'] = ['EXIT']
+                print("Disconnecting...")
+                send_message(client_socket,message=message)
+            shutdown_event.set()
+            client_socket.close()
+            message_receiver.join() #Join the thread back
+            #queue_handler.join()
+            break
+        else:
+            # Create message and send to the server to forward toward receiver(s)
+            message_ID = generate_message_ID(clientID=client_ID)
+            message = parse_create_message(content,clientID=client_ID,mssg_ID = message_ID)
+        
+        with lock:
+            if message['method']['action'] == 'DELETE':
+                MESSAGE_DICT[message['method']['ID']] = False
+    
+        #Acquire the lock to update the new message to the message dictionary
+        with lock:
+            MESSAGE_DICT[message_ID] = {
+                'message' : message,
+                'status' : True,
+            }
+        print(message)
+        send_message(client_socket,message=message)
+        
+        input_ready_event_dict[client_ID].clear()
+        
+   
+
 
 if __name__ == "__main__":
-    start_client()
-    
+    main()
--- server.py
+++ server.py
@@ -1,60 +1,237 @@
-#!/usr/bin/env python3
-
 import socket
+import select
+import json
+from _thread import *
 import threading
+import time
+import random
 
-# server configuration
-HOST = '127.0.0.1'
-PORT = 65432
+'''
+MESSAGE FORMAT
+    {
+        'sender'
+        'receiver'
+        'content'
+        'ID'
+        'method' {
+            'action'
+            'ID'
+        }
+    }
+'''
 
-# store connected clients
-clients = {}
-lock = threading.Lock() 
+# A simple code to get the host name of local machine and its IP address
+# host_name = socket.gethostname()
+# ip_address = socket.gethostbyname(host_name)
+# print(ip_address)
 
-def handle_client(conn, addr, client_name):
-    print(f"{client_name} connected from {addr}")
+IP = '127.0.0.1'
+PORT = 12000
+HEADER_LENGTH = 2048
+BACK_LOG = 100
+
+##Dictionary to store clients
+CLIENTS = {}
+IS_CLIENTS_CHANGED = True
+
+#create a lock with 2 status: locked and unlocked
+lock = threading.Lock()
+
+def receive_message(socket):
     try:
-        while True:
-            msg = conn.recv(1024).decode()
-            if not msg or msg.lower() == '.exit':
-                break
+        data = socket.recv(HEADER_LENGTH).decode('utf-8')
+        if not data:
+            # No data received means the client has disconnected
+            return None
+        return json.loads(data)
+    except (ConnectionResetError, ConnectionAbortedError, OSError):
+        # Handle when the client disconnects abruptly
+        return None
+
+def broadcast_message(socket,message):
+    """
+        socket: socket of receiver
+    """
+    socket.send(json.dumps(message).encode('utf-8'))
+
+def broadcast_client_list():
+    """
+    Broadcast the updated client list to all connected clients as a string.
+    """
+    # Create a string representation of the client list
+    client_list = ", ".join(CLIENTS.keys())  # Comma-separated client IDs
+    print(client_list)
+    message = {
+        'sender': 'server',
+        'receiver': None,
+        'content': client_list,  # Convert the client list to a single string
+        'ID': None,
+        'method': {
+            'action': 'UPDATE_CLIENT_LIST',
+            'ID': None
+        }
+    }
+    for client in CLIENTS.values():
+        try:
+            broadcast_message(client['socket'], message)
+            print("Broadcast sucessfully")
+        except Exception as e:
+            print(f"Error broadcasting to client {client['action']}: {e}")
+
+def generate_client_ID(name):
+    while True:
+        # Generate a random number between 1000 and 9999
+        random_number = random.randint(1000, 9999)
+        
+        # Create a unique ID by combining name and random number
+        uid = f"{name}_{random_number}"
+        
+        # Check if the ID is unique in the clients dictionary
+        if uid not in CLIENTS:
+            return uid
+        
+
+def create_new_client(client_socket):
+    message = {
+        'sender' : 'server',
+        'receiver': None,
+        'content': 'Please enter your user name',
+        'ID': None,
+        'method': {
+            'action' : 'NEW',
+            'ID' : None,
+        }
+    }
+    broadcast_message(client_socket,message=message)
+
+    message = receive_message(client_socket)
+
+    client_name = message.get('sender')
+    method_action = message.get('method',{}).get('action')
+
+    if client_name and method_action == 'NEW':
+        client_ID = generate_client_ID(client_name)
+
+    message = {
+        'sender' : 'server',
+        'receiver': [client_ID],
+        'content': client_ID,
+        'ID': None,
+        'method': {
+            'action' : 'NEW',
+            'ID' : None,
+        }
+    }
+
+    #Broad cast the assign ID back for the client
+    broadcast_message(client_socket,message=message)
+    return client_ID, client_name
+
+def remove_connection(client_ID):
+    with lock:
+        if client_ID in CLIENTS:
+            client_socket = CLIENTS[client_ID]['socket']
+            print(f"Removing client: {client_ID}")
+            del CLIENTS[client_ID]
+            try:
+                client_socket.close()
+            except Exception as e:
+                print(f"Error closing client socket {client_ID}: {e}")
+
+def handle_client_message(message):
+
+
+    sender = message.get("sender")
+    receivers = message.get("receiver")
+    mssg_ID = message.get("ID")
+    method = message.get("method", {})
+    action = method.get("action")
+    opt_ID = method.get("ID")
+
+    if action == 'POST' or action == 'REPLY' or action == 'DELETE':
+        #Send message to all available client in the server
+        if receivers[0] == 'all':
+            for client in CLIENTS:
+                broadcast_message(CLIENTS[client]['socket'],message=message)
+        else:
+            print(receivers)
+            for client in CLIENTS:
+                if client in receivers:
+                    broadcast_message(CLIENTS[client]['socket'],message=message)
             
-            # parse recipient and message (recipient: message)
-            if ':' in msg:
-                recipient, message = msg.split(':', 1)
-                message = message.strip()
-                
-                with lock:
-                    if recipient in clients:
-                        clients[recipient].sendall(f"{client_name}: {message}".encode())
-                        
-                    else:
-                        conn.sendall(f"User {recipient} not found.".encode())
-            else:
-                conn.sendall("Invalid message format. Use 'recipient: message'.".encode())
-    except Exception as e:
-        print(f"Error with {client_name}: {e}")
-    finally:
-        with lock:
-            del clients[client_name]
-        conn.close()
-        print(f"{client_name} disconnected")
-        
-def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind((HOST, PORT))
-        server.listen()
-        print(f"Server running on {HOST}:{PORT}")
-        
-        while True:
-            conn, addr = server.accept()
-            conn.sendall("Enter your name: ".encode())
-            client_name = conn.recv(1024).decode().strip()
-        
-            with lock:
-                clients[client_name] = conn
-            
-            threading.Thread(target=handle_client, args=(conn, addr, client_name), daemon=True).start()
+        return False
+    elif action == 'EXIT':
+        print("Client {} disconnected".format(CLIENTS[sender]['address']))
+        remove_connection(sender)
+        broadcast_client_list()
+        return True
+    
+
+
+    
+
+    
+
+
+
+def handle_client(client_socket, client_address):
+    # GENERATE UNIQUE ID FOR CLIENT
+    client_ID, client_name = create_new_client(client_socket=client_socket)
+
+        # Add new client to the available client list
+    with lock:
+        CLIENTS[client_ID] = {
+            'socket' : client_socket,
+            'address' : client_address,
+            'name' : client_name,
+            'message' : []
+        }
+
+        print('Add new connection from {}:{}, username:{}, ID:{}'.format(*client_address,client_name,client_ID))
+        broadcast_client_list()
+    
+    while True:
+        message = receive_message(client_socket)
+        if not message:
+            print("Client {} disconnected".format(*client_address))
+            remove_connection(client_ID)
+            broadcast_client_list()
+            break
+        print('Received message from {}:{}'.format(*client_address))
+        is_exit = handle_client_message(message=message)
+        if is_exit:
+            break
+
+
+
+
+
+def main():
+
+    #Create a socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #Reuse the address and port without waiting time, and bind multiple sockets to the same address
+    #Avoid "Address already in use"
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+    #bind
+    server_socket.bind((IP,PORT))
+
+    # BACK_LOG connection can be queued to be processed
+    server_socket.listen(BACK_LOG)
+
+    
+
+    print("The server is ready for connection!")
+    print(f"Listening for connection on {IP}:{PORT}...")
+
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_handler = threading.Thread(target=handle_client,args=(client_socket,client_address),daemon= True)
+        client_handler.start()
+
             
 if __name__ == "__main__":
-    start_server()
+    main()
